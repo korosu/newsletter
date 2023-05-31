2e4c8b6ebf7811b7ae763833ebca2d87944000f3
korosu <dmshijie@gmail.com>
2023-05-31 23:55:22 +0200

    goHome

--- src/app/complete/page.js
+++ src/app/complete/page.js
@@ -1,11 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Complete() {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
+  const router = useRouter();
+  function goHome() {
+    router.push("/");
+  }
   return (
     <div className="flex flex-col p-10 w-screen  bg-slate-100 h-full sm:h-auto gap-6 sm:max-w-xl sm:m-32 sm:rounded-3xl">
       <img
@@ -18,7 +22,10 @@ export default function Complete() {
         A confirmation email has been sent to {email}. Please open it and click
         the button inside to confirm your subscription
       </p>
-      <button className="hover:shadow-xl hover:text-red-50 hover:shadow-red-500/50 transition hover:bg-gradient-to-r from-orange-600 via-red-500 to-orange-600 self-center w-64 mt-auto bottom-10 text-slate-50 bg-slate-700 hover:bg-slate-600 h-12 rounded-md">
+      <button
+        onClick={goHome}
+        className="hover:shadow-xl hover:text-red-50 hover:shadow-red-500/50 transition hover:bg-gradient-to-r from-orange-600 via-red-500 to-orange-600 self-center w-64 mt-auto bottom-10 text-slate-50 bg-slate-700 hover:bg-slate-600 h-12 rounded-md"
+      >
         Dismiss message
       </button>
     </div>
