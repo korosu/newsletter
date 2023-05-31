7334343208d76ba245a94ca677fb3b80f7b95643
korosu <dmshijie@gmail.com>
2023-05-31 19:18:35 +0200

    f

--- src/app/complete/page.js
+++ src/app/complete/page.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 export default function complete() {
   return (
-    <div className="flex flex-col p-10 w-screen  bg-slate-100 h-full gap-6">
+    <div className="relative flex flex-col p-10 w-screen  bg-slate-100 h-full gap-6">
       <img
         src="assets/images/icon-success.svg"
         alt="svg image"
@@ -12,7 +12,7 @@ export default function complete() {
         A confirmation email has been sent to ash@loremcompany.com. Please open
         it and click the button inside to confirm your subscription
       </p>
-      <button className="self-center w-64 fixed bottom-10 text-slate-50 bg-slate-700 hover:bg-slate-600 h-12 rounded-md">
+      <button className="self-center w-64 absolute bottom-10 text-slate-50 bg-slate-700 hover:bg-slate-600 h-12 rounded-md">
         Dismiss message
       </button>
     </div>
